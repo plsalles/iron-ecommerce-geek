2cd3af8580095cdc7ce0e9533e77b9975bf85ddf
Henrique Mendes <henrique.gmendes@hotmail.com>
2020-05-26 18:33:31 -0300

    feat(form) implement form with intelligent validation

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Button as ButtonBase } from 'antd'
 
-const Button = ({ children, method }) => <button onClick={() => method()}>{children}</button>;
+const Button = ({ isLoading, children, method, type }) => <ButtonBase type="primary" loading={isLoading} htmlType={type || 'Button'} onClick={() => method && method()}>{children}</ButtonBase>;
 
 export const teste = () => {
   console.log('OI');
--- src/pages/SecondPage/SecondPage.js
+++ src/pages/SecondPage/SecondPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { GeneralTemplate } from '../../templates';
 import { Redirect } from 'react-router-dom';
+import { FormTest } from '../../components/molecules';
 
 const SecondPage = ({ loggedUser }) => {
   return loggedUser ? (
     <GeneralTemplate>
       NOSSO NOVO CONTEUDO
+      <FormTest />  
     </GeneralTemplate>
   ) : <Redirect to="/teste/NAO-AUTORIZADO!" />;
 };
